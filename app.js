3b2b1c9a7159bba53dd670469de7a1b5f19fc521
Henry Okon <henryok94@gmail.com>
2020-04-16 21:18:54 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//variables
+//Data types-string, integer, array doubleetc
+
+
+// - javascript variables 
+//- javascript Data types (strings and integers)
+
+//1. variables
+
+//containers or boxes 
+
+let name = "string"; // "" 
+let age = 25; 
+
+
+//console. log()
